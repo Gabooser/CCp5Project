1e9b21450ae8411a916327e62504f14d2d878f94
Le <leokalbermatter@gmail.com>
2025-03-31 10:33:42 +0200

    multiple canvases

--- sketch.js
+++ sketch.js
@@ -1,27 +1,36 @@
-function setup() {
-  createCanvas(400, 400);
-  noLoop();
-}
-function draw() {
-  let cols =10;
-  let rows = 10;
-  let squareSize = width / cols;
-   for (let i= 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let x = i * squareSize;
-      let y = j * squareSize;
+function sketch1(f) {
+  f.setup = function() {
+    f.createCanvas(400, 400);
+    f.noLoop();
+  };
 
-      let randomDirection = random() < 0.5;
-      strokeWeight(3);
-      if (randomDirection) {
-        line(x, y, x + squareSize, y + squareSize);
-      } else {
-        line(x + squareSize, y, x, y + squareSize);
+  f.draw = function() {
+    let cols =10;
+    let rows = 10;
+    let squareSize = f.width / cols;
+     for (let i= 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        let x = i * squareSize;
+        let y = j * squareSize;
+
+        let randomDirection = f.random() < 0.5;
+        f.strokeWeight(3);
+        if (randomDirection) {
+          f.line(x, y, x + squareSize, y + squareSize);
+        } else {
+          f.line(x + squareSize, y, x, y + squareSize);
+        }
       }
     }
-  }
+  };
+
+  f.mousePressed = function() {
+    f.redraw();
+  };
 }
 
-function mousePressed() {
-  redraw();
+new p5(sketch1);
+
+function sketch2(e) {
+
 }
