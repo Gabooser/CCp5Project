46fa435394e71e5afdf955ee5230643df4e1aa18
Le <leokalbermatter@gmail.com>
2025-03-31 09:41:53 +0200

    created a sketch with diagonals
    made a sketch that generates diagonals in a 10 x 10 square grid and makes them randomly face left or right.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(400, 400);
-}
+  let cols = 10;
+  let rows = 10;
+  let squareSize = width / cols;
+   for (let i= 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * squareSize;
+      let y = j * squareSize;
 
-function draw() {
-  background(220);
+      let randomDirection = random() < 0.5;
+      strokeWeight(3);
+      if (randomDirection) {
+        line(x, y, x + squareSize, y + squareSize);
+      } else {
+        line(x + squareSize, y, x, y + squareSize);
+      }
+    }
+   }
 }
